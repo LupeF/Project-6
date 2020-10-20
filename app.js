2295efadf78d86d4182475d4d9cf96e825c47ae0
LupeF <46634202+LupeF@users.noreply.github.com>
2020-10-19 22:32:13 -0700

    added transition and transform to display

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const keyboard = document.getElementById('qwerty');
 let phraseDiv = document.getElementById('phrase');
 const btnReset = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
-let tries = document.querySelectorAll('img');
-let ul = document.querySelector('ul');
+let hearts = document.querySelectorAll('img');
+let newTitle = document.querySelector('.title').textContent = "Guess the hero";
 let missed = 0;
-const phrases = [   'hello world',
-                    'this is an element',
-                    'treehouse',
-                    'javascript',
-                    'create an element'  
+const phrases = [   'Spider Man',
+                    'Iron Man',
+                    'Wolverine',
+                    'Batman',
+                    'Hulk',  
 ];
+
 
 btnReset.addEventListener('click', () =>{
     overlay.style.display = "none"; //hides overlaybackground when start is clicked
+    let newHeader = document.querySelector('.header');
+    newHeader.textContent = "Who am I?"
 
 })
 
@@ -39,24 +42,22 @@ const addPhraseToDisplay = (arr) => {
     }
 }
 
-addPhraseToDisplay(phraseArray); //calls function to add phrase to display
+addPhraseToDisplay(phraseArray); //calls the function to add a phrase to the display
 
 const checkLetter = (button) => {
-    let ul = document.querySelector('ul').style.perspective = "400px";
     let letter = document.querySelectorAll('ul .letter'); //selects li with class of letter
     let match = null;
     for (let i =0; i < letter.length; i++) { // loops through al li elements 
-        if (letter[i].textContent === button.textContent){ //compares the text of li with the button
+        if (letter[i].textContent.toLowerCase() === button.textContent){ //compares the text of li with the button
             letter[i].className += " show"; //adds class name to li
-            letter[i].style.transform = "rotateY(360deg)";
             match += letter[i].textContent; // adds textcontent of letter to a variable
-
         } 
     }
     return match;
 }
 
-keyboard.addEventListener('click', (e) => { //adds eventlistener on keyboard div
+//adds eventlistener on keyboard div
+keyboard.addEventListener('click', (e) => { 
     if (e.target.tagName === 'BUTTON'){    
         e.target.className = "chosen"; //adds class of chosen if button is clicked
     } if (e.target.className === 'chosen'){
@@ -64,13 +65,14 @@ keyboard.addEventListener('click', (e) => { //adds eventlistener on keyboard div
         let letterFound = checkLetter(e.target); 
         if (letterFound === null){
             missed += 1;
-            tries[missed - 1 ].style.display= "none";
+            hearts[missed - 1 ].src = "images/lostHeart.png"; // removes the hearts
         }
         checkWin();
     }  
 
 });
 
+//**restart */
 const restart = () => {
     missed = 0;
     let phraseUl = document.querySelector("ul");
@@ -81,17 +83,19 @@ const restart = () => {
     if (btnReset.textContent === "try again"){
         btnReset.addEventListener('click', (e) =>{
             phraseUl.innerHTML = "";  // erase's the li's in the ul
+            //Resests buttons
             for (let i = 0; i < buttons.length; i++ ){
                 if (buttons[i].className === "chosen"){
                     buttons[i].classList.remove("chosen");
                     buttons[i].removeAttribute("disabled");
-
                 } 
             }
-            for (let i = 0; i < tries.length; i++) {
-                tries[i].style.display = "block";
+            // Resets hearts
+            for (let i = 0; i < hearts.length; i++) {
+                hearts[i].src = "images/liveHeart.png";
                 
             } 
+            //adds new phrase
             addPhraseToDisplay(newArray);
         })
     }
